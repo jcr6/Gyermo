70d7531fdf0d02826f7427266caab12803364e2e
Jeroen Broks <11202073+Tricky1975@users.noreply.github.com>
2024-11-24 11:20:19 +0100

    Jalondi Build script

---
+++ Gyermo/Gyermo_Jalondi.lua
@@ -0,0 +1,40 @@
+-- License:
+-- 
+-- Gyermo
+-- Jalondi Asset Pack Script
+-- 
+-- 
+-- 
+-- 	(c) Jeroen P. Broks, 2024
+-- 
+-- 		This program is free software: you can redistribute it and/or modify
+-- 		it under the terms of the GNU General Public License as published by
+-- 		the Free Software Foundation, either version 3 of the License, or
+-- 		(at your option) any later version.
+-- 
+-- 		This program is distributed in the hope that it will be useful,
+-- 		but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- 		MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- 		GNU General Public License for more details.
+-- 		You should have received a copy of the GNU General Public License
+-- 		along with this program.  If not, see <http://www.gnu.org/licenses/>.
+-- 
+-- 	Please note that some references to data like pictures or audio, do not automatically
+-- 	fall under this licenses. Mostly this is noted in the respective files.
+-- 
+-- Version: 24.11.23
+-- End License
+Jalondi.Doing("Scripting","Jalondi")
+Jalondi.Doing("Platform",Jalondi.Platform)
+
+if Jalondi.Platform=="Windows" then
+	BaseDir="/Projects"
+elseif Jalondi.Platform=="Linux" then
+	error("Linux Scripting not yet properly set up")
+else
+	error("Unknown platform")
+end
+
+Jalondi.Add(BaseDir.."/Fonts/DosFont/DosFont.jfbf","Fonts/DOS.jfbf","zlib","","")
+Jalondi.Add("Assets","","zlib")
+Jalondi.Start("../Exe/"..Jalondi.Platform.."/Gyermo.jcr")
