9e6223cf55d21f180a4e08c8674fa3bbcecddcba
Jeroen Broks <11202073+Tricky1975@users.noreply.github.com>
2024-11-25 10:08:57 +0100

    Update Gyermo_GUI.cpp

--- Gyermo/Gyermo_GUI.cpp
+++ Gyermo/Gyermo_GUI.cpp
@@ -22,7 +22,7 @@
 // 	Please note that some references to data like pictures or audio, do not automatically
 // 	fall under this licenses. Mostly this is noted in the respective files.
 // 
-// Version: 24.11.24 I
+// Version: 24.11.25
 // End License
 #include <map>
 #include <june19.hpp>
@@ -30,6 +30,8 @@
 #include "Gyermo_GUI.hpp"
 #include "Gyermo_Assets.hpp"
 #include "Gyermo_Config.hpp"
+#include "Gyermo_ReadJCR.hpp"
+
 using namespace Slyvina::TQSG;
 using namespace Slyvina::TQSE;
 using namespace Slyvina::Units;
@@ -47,21 +49,24 @@ namespace Slyvina {
 
 			static String UI_CurrentTab{ "Data" };
 			static std::map<String,j19gadget*> UI_TabPanels{};
-			std::map<String, j19gadget*> UI_DataFields{};
+			std::map<String, j19gadget*>
+				UI_DataFields{},
+				UI_BlockFields{};
 			static TImageFont UI_Font{ nullptr };
 			static j19gadget
 				* UI_Work{ nullptr },
 				* UI_Background{ nullptr },
 				* UI_Header{ nullptr },
 				* UI_Left{ nullptr },
+				* UI_TabData{ nullptr },
+				* UI_TabBlock{ nullptr },
+				* UI_BlckNoBlock{ nullptr },
+				* UI_TabView{ nullptr };
+			j19gadget
 				* UI_Right{ nullptr },
 				* UI_Resource{ nullptr },
 				* UI_Directory{ nullptr },
 				* UI_FileList{ nullptr },
-				* UI_TabData{ nullptr },
-				* UI_TabBlock{ nullptr },
-				* UI_TabView{ nullptr };
-			j19gadget
 				* UI_DataEntry{ nullptr },
 				* UI_DataMain{ nullptr },
 				* UI_DataType{ nullptr },
@@ -71,13 +76,22 @@ namespace Slyvina {
 				* UI_DataAlias{ nullptr },
 				* UI_DataOtherLabel{ nullptr },
 				* UI_DataOther{ nullptr },
-				* UI_DataResourceType{ nullptr };
+				* UI_DataResourceType{ nullptr },
+				* UI_BlckGroup{ nullptr },
+				* UI_BlckEntry{ nullptr },
+				* UI_BlckID{ nullptr },
+				* UI_BlckEntries{ nullptr },
+				* UI_BlckEntryOffset{ nullptr },
+				* UI_BlckRatio{ nullptr },
+				* UI_BlckOther{ nullptr };
 			static UniqueArray<j19gadget*> UI_Tabs{};
 
 			static void DrawRight(j19gadget*, j19action) {
 				for (size_t i = 1; i < UI_Tabs.size(); ++i) UI_Tabs[i]->X(UI_Tabs[i - 1]->X() + UI_Tabs[i - 1]->W());
 			}
-
+			static void AutoNoBlock(j19gadget*, j19action) {
+				UI_BlckNoBlock->Visible = !UI_BlckGroup->Visible;
+			}
 			static void DrawTab(j19gadget* g, j19action) {
 				auto Panel{ UI_TabPanels[g->Caption] };
 				Panel->Visible = g->Caption == UI_CurrentTab; 
@@ -94,7 +108,7 @@ namespace Slyvina {
 			static void AttachImage(j19gadget* p, String ImageFile) {
 				if (Assets()->EntryExists(ImageFile)) {
 					QCol->Doing("Loading", ImageFile);
-					auto Pic{ LoadImage(Assets(),"GFX/Crystal.png") };
+					auto Pic{ LoadImage(Assets(),ImageFile) };
 					int W{ (int)floor((double)Pic->Width() * ((double)p->H() / (double)Pic->Height())) };
 					QCol->Doing("Size", TrSPrintF("%dx%d", W, p->H()));
 					auto Gad{ CreatePicture(p->W() - W,0,W,p->H(),p,Pic_FullStretch) };
@@ -107,6 +121,7 @@ namespace Slyvina {
 
 #define DataTab 150
 #define DataField(gadget,Caption) ColorGadget(CreateLabel(Caption,2,y,DataTab,HeadHeight,CPan),"Tab_Data"); gadget=CreateLabel("--",DataTab,y,CPan->W(),HeadHeight,CPan); ColorGadget(gadget,"FLD_DATA","Yellow","Black"); y+=HeadHeight
+#define BlckField(gadget,Caption) ColorGadget(CreateLabel(Caption,2,y,DataTab,HeadHeight,UI_BlckGroup),"Tab_Block"); gadget=CreateLabel("--",DataTab,y,CPan->W(),HeadHeight,UI_BlckGroup); ColorGadget(gadget,"FLD_BLOCK","180,255,0","Black"); y+=HeadHeight
 
 			void UI_Init() {				
 				const int HeadHeight{ 15 };
@@ -136,6 +151,7 @@ namespace Slyvina {
 				UI_Left = CreateGroup(0, HeadHeight * 3, UI_Work->W() / 2, UI_Work->H() - (HeadHeight * 3), UI_Work);
 				UI_Right = CreateGroup(UI_Work->W() / 2, HeadHeight * 3, UI_Work->W() / 2, UI_Work->H() - (HeadHeight * 3), UI_Work);
 				UI_FileList = CreateListBox(5, 5, UI_Left->W() - 35, UI_Left->H() - 100, UI_Left);
+				UI_FileList->CBAction = Act_FileList;
 				ColorGadget(UI_FileList, "FileList", "White", "BrightBlue");
 				UI_TabData = CreateButton("Data", 0, 0, UI_Right);
 				UI_TabData->Enabled = true;
@@ -156,8 +172,10 @@ namespace Slyvina {
 					DrawTab(UI_Tabs[i], j19action::Activate);
 				}
 				ActTab(UI_TabData, j19action::Activate);
+
+				// Tab: Data
 				auto CPan{ UI_TabPanels["Data"] };
-				AttachImage(CPan, "GFX/Crystal.png");
+				AttachImage(CPan, "GFX/Crystal.png");				
 				auto y{ 2 };
 				DataField(UI_DataEntry, "Entry:"); UI_DataFields["$__Entry"] = UI_DataEntry;
 				DataField(UI_DataMain, "Main File:");
@@ -173,6 +191,7 @@ namespace Slyvina {
 				DataField(UI_DataFields["$__OS"], "Platform:");
 				DataField(UI_DataFields["$__Author"], "Author:");
 				DataField(UI_DataFields["$__Notes"], "Notes:");
+				DataField(UI_DataFields["$__MD5HASH"], "MD5");
 				UI_DataAliasLabel = CreateLabel("Aliases:", 2, y, DataTab, HeadHeight, CPan);
 				UI_DataAlias = CreateListBox(DataTab, y, CPan->W() - (DataTab + 100), HeadHeight * 4, CPan);
 				ColorGadget(UI_DataAliasLabel, "Tab_Data");
@@ -184,6 +203,34 @@ namespace Slyvina {
 				ColorGadget(UI_DataOther, "Other", "Pink", "Red");
 				y += (HeadHeight * 4) + 2;
 				DataField(UI_DataResourceType, "Resource Type:");
+
+				// Tab: Block
+				CPan = UI_TabPanels["Block"];
+				CPan->CBDraw = AutoNoBlock;
+				AttachImage(CPan, "GFX/Wendicka.png");
+				UI_BlckNoBlock = CreateLabel("This entry is not part of a block", 0, 0, 0, 0, CPan); ColorGadget(UI_BlckNoBlock, "Tab_Block");
+				UI_BlckNoBlock->Visible = false;
+				UI_BlckGroup = CreateGroup(0, 0, CPan->W(), CPan->H(), CPan);
+				y = 2;
+				BlckField(UI_BlckEntry, "Entry:");
+				BlckField(UI_BlckID, "Block ID:");
+				//printf("DEBUG BLCKID: %d:> (%d,%d); FG:%02x%02x%02x;FA:%02x\n", UI_BlckID->ID(), UI_BlckID->X(), UI_BlckID->Y(), UI_BlckID->FR, UI_BlckID->FG, UI_BlckID->FB, UI_BlckID->FA);
+				ColorGadget(CreateLabel("Entries:", 2, y, DataTab, HeadHeight, UI_BlckGroup), "Tab_Block");
+				UI_BlckEntries = CreateListBox(DataTab, y,  CPan->W() - (DataTab + 200), HeadHeight * 4, UI_BlckGroup);
+				ColorGadget(UI_BlckEntries, "BLOCK_ENTRIES", "BrightCyan", "Cyan");
+				y += HeadHeight * 4;
+				BlckField(UI_BlckEntryOffset, "Entry Offset:");
+				BlckField(UI_BlockFields["%__Offset"], "Block Offset:");
+				BlckField(UI_BlockFields["%__Size"], "Block Size:");
+				BlckField(UI_BlockFields["%__CSize"], "Block Comp. Size:");
+				BlckField(UI_BlockFields["$__Storage"], "Block Storage:");
+				BlckField(UI_BlckRatio, "Block Ratio:");
+				BlckField(UI_BlockFields["$__JCR6FOR"], "JCR6 Library:");
+				BlckField(UI_BlockFields["$__MD5HASH"], "MD5:");
+				ColorGadget(CreateLabel("Other Data:", 2, y, DataTab, HeadHeight, UI_BlckGroup), "Tab_Block");
+				UI_BlckOther = CreateListBox(DataTab, y, CPan->W() - (DataTab + 200), HeadHeight * 4, UI_BlckGroup);
+				ColorGadget(UI_BlckOther, "Other");
+
 			}
 
 
